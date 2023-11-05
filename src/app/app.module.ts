19ae6fdd9125799601ab84e074797f0ae6e77735
khaledabdelmoaty <42172644+khaledabdelmoaty@users.noreply.github.com>
2023-11-05 20:16:30 +0300

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { PalylistComponent } from './Pages/palylist/palylist.component';
+import { SearchComponent } from './Pages/search/search.component';
+import { SignupComponent } from './Pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    PalylistComponent,
+    SearchComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
